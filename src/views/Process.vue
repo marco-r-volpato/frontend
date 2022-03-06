13f22448785695a2115a6ebfea9867f57fdd334a
winemasterclass <marco@winemasterclass.com>
2022-03-06 15:31:12 +1100

    Added Axios & Setup API GET Processes

--- src/views/Process.vue
+++ src/views/Process.vue
@@ -1,3 +1,31 @@
 <template>
-    Process: {{$route.params.id}}
+  <h1 v-if="process.id">Process: {{ process.attributes.title }}</h1>
+  <h1 v-else-if="error">{{ error }}</h1>
+  <h1 v-else class="text-slate-500">Loading...</h1>
 </template>
+
+<script>
+import axios from "axios";
+
+export default {
+  async mounted() {
+    try {
+      const response = await axios.get(
+        "http://localhost:1337/api/processes/" +
+          this.$route.params.id +
+          "?fields=title,context"
+      );
+      this.process = response.data.data;
+      console.log(this.process);
+    } catch (error) {
+      this.error = error;
+    }
+  },
+  data() {
+    return {
+      process: {},
+      error: null,
+    };
+  },
+};
+</script>
--- src/views/Processes.vue
+++ src/views/Processes.vue
@@ -1,12 +1,33 @@
 <template>
-    <div v-for="process in processes" :key="process.id">
-       <router-link :to="{ name: 'Process', params: { id: process.id }}">{{process.name}}</router-link>
-    </div>
+  <div v-if="processes">
+    <h1 v-for="process in processes" :key="process.id">
+      <router-link :to="{ name: 'Process', params: { id: process.id } }">{{ process.attributes.title }}</router-link>
+    </h1>
+  </div>
+  <h1 v-else-if="error">{{ error }}</h1>
+  <h1 v-else class="text-slate-500">Loading...</h1>
 </template>
 
-<script setup>
-const processes = [
-  { name: 'Process 1', id: '1'},
-  { name: 'Process 2', id: '2' },
-]
+<script>
+import axios from "axios";
+
+export default {
+  async mounted() {
+    try {
+      const response = await axios.get(
+        "http://localhost:1337/api/processes?fields=title"
+      );
+      this.processes = response.data.data;
+      console.log(this.processes);
+    } catch (error) {
+      this.error = error;
+    }
+  },
+  data() {
+    return {
+      processes: [],
+      error: null,
+    };
+  },
+};
 </script>
